3c217bc89555e20c4c553c695bb459365765ed58
Lali <csomorl@kkszki.hu>
2024-10-01 13:56:07 +0200

    database and records added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 
+
 namespace ProductAPI
 {
     public class Program
--- obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802606858b6fa9dc281da88fdfd825982978375f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
